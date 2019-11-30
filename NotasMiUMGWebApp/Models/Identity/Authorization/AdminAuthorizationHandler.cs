586b188550057c064f12c28a4df2d8499f5e0ab3
Juan Guerra <juanguerra97@protonmail.com>
2019-11-30 15:47:43 -0600

    AuthorizationHandler para el rol de administrador

---
+++ NotasMiUMGWebApp/Models/Identity/Authorization/AdminAuthorizationHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+
+namespace NotasMiUmg.Authorization
+{
+    public class AdminAuthorizationHandler :
+        AuthorizationHandler<OperationAuthorizationRequirement>
+    {
+        protected override Task
+            HandleRequirementAsync(AuthorizationHandlerContext context,
+                                   OperationAuthorizationRequirement requirement)
+        {
+            if (context.User == null)
+            {
+                return Task.CompletedTask;
+            }
+
+            if (requirement.Name != Constants.CrudCarreraOperationName &&
+                requirement.Name != Constants.CrudPensumOperationName &&
+                requirement.Name != Constants.CrudCursoOperationName &&
+                requirement.Name != Constants.CrudPensumCursoOperationName)
+            {
+                return Task.CompletedTask;
+            }
+
+            if (context.User.IsInRole(RoleNameConstants.RolAdministrador))
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
