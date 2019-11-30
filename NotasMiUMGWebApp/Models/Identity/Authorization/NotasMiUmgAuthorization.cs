23e230fff9bc337ba55fc3d552d3f09a0fa8ad47
Juan Guerra <juanguerra97@protonmail.com>
2019-11-30 15:27:49 -0600

    Constantes relacionadas con la autorizacion de usuarios

---
+++ NotasMiUMGWebApp/Models/Identity/Authorization/NotasMiUmgAuthorization.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Authorization.Infrastructure;
+
+namespace NotasMiUmg.Authorization
+{
+    public static class CarreraOperations
+    {
+        public static OperationAuthorizationRequirement Create =
+          new OperationAuthorizationRequirement { Name = OperationConstants.CreateOperationName };
+        public static OperationAuthorizationRequirement Read =
+          new OperationAuthorizationRequirement { Name = OperationConstants.ReadOperationName };
+        public static OperationAuthorizationRequirement Update =
+          new OperationAuthorizationRequirement { Name = OperationConstants.UpdateOperationName };
+        public static OperationAuthorizationRequirement Delete =
+          new OperationAuthorizationRequirement { Name = OperationConstants.DeleteOperationName };
+
+    }
+
+    public static class PensumOperations
+    {
+        public static OperationAuthorizationRequirement Create =
+          new OperationAuthorizationRequirement { Name = OperationConstants.CreateOperationName };
+        public static OperationAuthorizationRequirement Read =
+          new OperationAuthorizationRequirement { Name = OperationConstants.ReadOperationName };
+        public static OperationAuthorizationRequirement Update =
+          new OperationAuthorizationRequirement { Name = OperationConstants.UpdateOperationName };
+        public static OperationAuthorizationRequirement Delete =
+          new OperationAuthorizationRequirement { Name = OperationConstants.DeleteOperationName };
+
+    }
+
+    public static class CursoOperations
+    {
+        public static OperationAuthorizationRequirement Create =
+          new OperationAuthorizationRequirement { Name = OperationConstants.CreateOperationName };
+        public static OperationAuthorizationRequirement Read =
+          new OperationAuthorizationRequirement { Name = OperationConstants.ReadOperationName };
+        public static OperationAuthorizationRequirement Update =
+          new OperationAuthorizationRequirement { Name = OperationConstants.UpdateOperationName };
+        public static OperationAuthorizationRequirement Delete =
+          new OperationAuthorizationRequirement { Name = OperationConstants.DeleteOperationName };
+
+    }
+
+    public static class PensumCursoOperations
+    {
+        public static OperationAuthorizationRequirement Create =
+          new OperationAuthorizationRequirement { Name = OperationConstants.CreateOperationName };
+        public static OperationAuthorizationRequirement Read =
+          new OperationAuthorizationRequirement { Name = OperationConstants.ReadOperationName };
+        public static OperationAuthorizationRequirement Update =
+          new OperationAuthorizationRequirement { Name = OperationConstants.UpdateOperationName };
+        public static OperationAuthorizationRequirement Delete =
+          new OperationAuthorizationRequirement { Name = OperationConstants.DeleteOperationName };
+
+    }
+
+    public static class NotaOperations
+    {
+        public static OperationAuthorizationRequirement Create =
+          new OperationAuthorizationRequirement { Name = OperationConstants.CreateOperationName };
+        public static OperationAuthorizationRequirement Read =
+          new OperationAuthorizationRequirement { Name = OperationConstants.ReadOperationName };
+        public static OperationAuthorizationRequirement Update =
+          new OperationAuthorizationRequirement { Name = OperationConstants.UpdateOperationName };
+        public static OperationAuthorizationRequirement Delete =
+          new OperationAuthorizationRequirement { Name = OperationConstants.DeleteOperationName };
+
+    }
+
+    public static class EstudianteOperations
+    {
+        public static OperationAuthorizationRequirement Create =
+          new OperationAuthorizationRequirement { Name = OperationConstants.CreateOperationName };
+        public static OperationAuthorizationRequirement Read =
+          new OperationAuthorizationRequirement { Name = OperationConstants.ReadOperationName };
+        public static OperationAuthorizationRequirement Update =
+          new OperationAuthorizationRequirement { Name = OperationConstants.UpdateOperationName };
+        public static OperationAuthorizationRequirement Delete =
+          new OperationAuthorizationRequirement { Name = OperationConstants.DeleteOperationName };
+
+    }
+
+    /// <summary>
+    /// Nombres de las operaciones que se pueden hacer en una tabla
+    /// </summary>
+    public class OperationConstants
+    {
+        public static readonly string CreateOperationName = "Create";
+        public static readonly string ReadOperationName = "Read";
+        public static readonly string UpdateOperationName = "Update";
+        public static readonly string DeleteOperationName = "Delete";
+
+    }
+
+    /// <summary>
+    /// Nombres de los roles de usuarios
+    /// </summary>
+    public class RoleNameConstants
+    {
+        public static readonly string RolAdministrador = "ADMIN";
+        public static readonly string RolEstudiante = "ESTUDIANTE";
+    }
+
+}
