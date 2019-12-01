891f188985ba907eb77fdc9dc342d955b3a395b6
Juan Guerra <juanguerra97@protonmail.com>
2019-12-01 08:32:57 -0600

    Handler para la autorizacion en un estudiante

---
+++ NotasMiUMGWebApp/Models/Identity/Authorization/EstudianteAuthorizationHandler.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using jguerra.notasmiumg;
+
+namespace NotasMiUmg.Authorization
+{
+
+    /// <summary>
+    /// Handler para saber si un usuario puede modificar un registro de estudiante
+    /// </summary>
+    public class EstudianteAuthorizationHandler : AuthorizationHandler<OperationAuthorizationRequirement, Estudiante>
+    {
+
+        private UserManager<IdentityUser> _userManager;
+
+        public EstudianteAuthorizationHandler(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        protected override Task
+            HandleRequirementAsync(AuthorizationHandlerContext context,
+                                   OperationAuthorizationRequirement requirement,
+                                   Estudiante resource)
+        {
+            if (context.User == null || resource == null)
+            {
+                return Task.CompletedTask;
+            }
+
+
+            if (requirement.Name != Constants.CrudEstudianteOperationName)
+            {
+                return Task.CompletedTask;
+            }
+            
+            if (resource.UsuarioEstudiante.Id == _userManager.GetUserId(context.User))
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+
+    }
+}
