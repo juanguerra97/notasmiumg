6eb12d537561f5947b434586ed0848128cb254b1
Juan Guerra <juanguerra97@protonmail.com>
2019-12-01 08:37:40 -0600

    Handler para la autorizacion de Notas

---
+++ NotasMiUMGWebApp/Models/Identity/Authorization/NotaAuthorizationHandler.cs
@@ -0,0 +1,46 @@
+﻿
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using jguerra.notasmiumg;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+
+namespace NotasMiUmg.Authorization
+{
+    public class NotaAuthorizationHandler : AuthorizationHandler<OperationAuthorizationRequirement, Nota>
+    {
+
+        private UserManager<IdentityUser> _userManager;
+
+        public NotaAuthorizationHandler(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        protected override Task
+            HandleRequirementAsync(AuthorizationHandlerContext context,
+                                   OperationAuthorizationRequirement requirement,
+                                   Nota resource)
+        {
+            if (context.User == null || resource == null)
+            {
+                return Task.CompletedTask;
+            }
+
+
+            if (requirement.Name != Constants.CrudNotaOperationName)
+            {
+                return Task.CompletedTask;
+            }
+
+            if (resource.Estudiante.UsuarioEstudiante.Id == _userManager.GetUserId(context.User))
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+
+
+    }
+}
