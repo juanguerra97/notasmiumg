c6c3a03b3aee28bce38539e7809f91f805e4feb5
Juan Guerra <juanguerra97@protonmail.com>
2019-12-03 17:36:12 -0600

    Modificacion de requerimientos para contraseñas

--- NotasMiUMGWebApp/Startup.cs
+++ NotasMiUMGWebApp/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NotasMiUmg.Authorization;
 using Microsoft.AspNetCore.Authorization;
+using System;
 
 namespace NotasMiUMGWebApp
 {
@@ -52,6 +53,28 @@ namespace NotasMiUMGWebApp
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/dist";
+            });
+
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings.
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+
+                // Lockout settings.
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+                // User settings.
+                options.User.AllowedUserNameCharacters =
+                "abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = true;
             });
 
             services.AddScoped<IAuthorizationHandler, AdminAuthorizationHandler>();
