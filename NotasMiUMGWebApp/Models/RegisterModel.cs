236ae9059ecdfb8a3d667f7beaffe8b4bc539f9d
Juan Guerra <juanguerra97@protonmail.com>
2019-12-04 10:36:19 -0600

    Modelo de registro de un estudiante

---
+++ NotasMiUMGWebApp/Models/RegisterModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NotasMiUMGWebApp.Models
+{
+    public class RegisterModel
+    {
+
+        [Required(ErrorMessage = "Falta nombre de usuario")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Falta contraseña")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Falta el carné")]
+        public string Carne { get; set; }
+
+        [Required(ErrorMessage = "Falta el nombre")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Falta el apellido")]
+        public string Apellido { get; set; }
+
+        [Required(ErrorMessage = "Falta el año de inicio")]
+        public uint AnoInicio { get; set; }
+
+        [Required(ErrorMessage = "Falta el código de la carrera")]
+        public uint CodigoCarrera { get; set; }
+
+        [Required(ErrorMessage = "Falta el año del pensum")]
+        public uint AnoPensum { get; set; }
+
+    }
+}
