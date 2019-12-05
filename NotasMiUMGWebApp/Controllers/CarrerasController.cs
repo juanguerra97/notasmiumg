950430d99a83ca09480c8221b7f5d1981b722c7f
Juan Guerra <juanguerra97@protonmail.com>
2019-12-05 11:56:03 -0600

    Se estandariza el formato de las respuestas

--- NotasMiUMGWebApp/Controllers/CarrerasController.cs
+++ NotasMiUMGWebApp/Controllers/CarrerasController.cs
@@ -26,11 +26,12 @@ namespace NotasMiUMGWebApp.Controllers
         [Route("")]
         public async Task<IActionResult> GetAll()
         {
-            return Ok(new { 
-                status = 200, 
+            return Ok(new {
+                status = 200,
+                message = "Carreras",
                 data = _context.Carreras
                     .Select(c => new { c.CodigoCarrera, c.NombreCarrera })
-            });
+            }); ;
         }
 
         [HttpGet]
@@ -41,11 +42,19 @@ namespace NotasMiUMGWebApp.Controllers
                 .FirstOrDefault(c => c.CodigoCarrera == codigo);
             if(carrera != null)
             {
-                return Ok(new { data = carrera });
+                return Ok(new {
+                    status = 200,
+                    message = "Carrera",
+                    data = carrera
+                }); ;
             }
             else
             {
-                JsonResult resp = Json(new { });
+                JsonResult resp = Json(new {
+                    status = 400,
+                    message = "No se encontró la carrera",
+                    error = "La carrera no existe"
+                });
                 resp.StatusCode = 400;
                 return resp;
             }
@@ -59,7 +68,10 @@ namespace NotasMiUMGWebApp.Controllers
             var carrera = await _context.Carreras.FirstOrDefaultAsync(c => c.CodigoCarrera == model.CodigoCarrera);
             if(carrera != null)
             {
-                JsonResult resp = Json(new { message = "No se guardó la carrera", error = "Código duplicado" });
+                JsonResult resp = Json(new { 
+                    status = 400,
+                    message = "No se guardó la carrera", 
+                    error = "Código duplicado" });
                 resp.StatusCode = 400;
                 return resp;
             }
@@ -67,7 +79,10 @@ namespace NotasMiUMGWebApp.Controllers
             carrera = await _context.Carreras.FirstOrDefaultAsync(c => c.NombreCarrera.Equals(model.NombreCarrera, StringComparison.InvariantCultureIgnoreCase));
             if (carrera != null)
             {
-                JsonResult resp = Json(new { message = "No se guardó la carrera", error = "Nombre duplicado" });
+                JsonResult resp = Json(new { 
+                    status = 400,
+                    message = "No se guardó la carrera", 
+                    error = "Nombre duplicado" });
                 resp.StatusCode = 400;
                 return resp;
             }
@@ -75,10 +90,15 @@ namespace NotasMiUMGWebApp.Controllers
             {
                 await _context.Carreras.AddAsync(model);
                 await _context.SaveChangesAsync();
-                return Ok(new { message = "Carrera guardada" });
+                return Ok(new { 
+                    status = 200,
+                    message = "Carrera guardada" });
             } catch(DbUpdateException ex)
             {
-                JsonResult resp = Json(new { message = "No se guardó la carrera", error = ex.InnerException.Message });
+                JsonResult resp = Json(new { 
+                    status = 400,
+                    message = "No se guardó la carrera", 
+                    error = ex.InnerException.Message });
                 resp.StatusCode = 400;
                 return resp;
             }
