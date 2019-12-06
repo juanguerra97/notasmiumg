aed4ece34a174cfa53b49bf6bbb5438d705c0a7f
Juan Guerra <juanguerra97@protonmail.com>
2019-12-06 10:32:01 -0600

    Se verifica manualmente que solo usuarios ADMIN puedan crear nuevas carreras

--- NotasMiUMGWebApp/Controllers/CarrerasController.cs
+++ NotasMiUMGWebApp/Controllers/CarrerasController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using jguerra.notasmiumg;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using NotasMiUMGWebApp.Data;
@@ -15,10 +17,12 @@ namespace NotasMiUMGWebApp.Controllers
     {
 
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public CarrerasController(ApplicationDbContext context)
+        public CarrerasController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         [AllowAnonymous]
@@ -62,11 +66,19 @@ namespace NotasMiUMGWebApp.Controllers
             
         }
 
-        [Authorize(Roles = "ADMIN")]
+        [Authorize]
         [HttpPost]
         [Route("")]
         public async Task<IActionResult> Create([FromBody] Carrera model)
         {
+
+            var idUsuario = User.Claims.Where(a => a.Type == ClaimTypes.NameIdentifier).FirstOrDefault().Value;
+            bool esAdmin = await _userManager.IsInRoleAsync(await _userManager.FindByIdAsync(idUsuario), "ADMIN");
+            if (!esAdmin)
+            {
+                return Forbid();
+            }
+
             var carrera = await _context.Carreras.FirstOrDefaultAsync(c => c.CodigoCarrera == model.CodigoCarrera);
             if(carrera != null)
             {
