9d690ebfb072a0f5671b7bb5464306f1f3e8b0fa
Juan Guerra <juanguerra97@protonmail.com>
2019-12-06 18:09:56 -0600

    Se utiliza BadRequest() en lugar de Json() al devolver errores

--- NotasMiUMGWebApp/Controllers/RegisterController.cs
+++ NotasMiUMGWebApp/Controllers/RegisterController.cs
@@ -32,49 +32,45 @@ namespace NotasMiUMGWebApp.Controllers
             var user = await _userManager.FindByNameAsync(model.Username);
             if(user != null)
             {
-                JsonResult resp = Json(new { 
-                    status = 400, 
-                    message = "No se puedo guardar el usuario" ,
-                    error  = "Nombre de usuario repetido"
+                return BadRequest(new
+                {
+                    status = 400,
+                    message = "No se puedo guardar el usuario",
+                    error = "Nombre de usuario duplicado"
                 });
-                resp.StatusCode = 400;
-                return resp;
             }
 
             var student = _context.Estudiantes.FirstOrDefault(e => e.Carne == model.Carne);
             if(student != null)
             {
-                JsonResult resp = Json(new { 
-                    status = 400, 
+                return BadRequest(new
+                {
+                    status = 400,
                     message = "No se pudo guardar el estudiante",
                     error = "Carné duplicado"
                 });
-                resp.StatusCode = 400;
-                return resp;
             }
 
             var pensum = _context.Pensums.FirstOrDefault(p => p.CodigoCarrera == model.CodigoCarrera && p.AnoPensum == model.AnoPensum);
             if(pensum == null)
             {
-                JsonResult resp = Json(new { 
+                return BadRequest(new
+                {
                     status = 400,
                     message = "No se pudo guardar el estudiante",
                     error = "El pensum no existe"
                 });
-                resp.StatusCode = 400;
-                return resp;
             }
 
             var identityUser = await _userManager.CreateAsync(new IdentityUser { Email = model.Username }, model.Password);
             if (!identityUser.Succeeded)
             {
-                JsonResult resp = Json(new { 
+                return BadRequest(new
+                {
                     status = 400,
                     message = "No se puedo crear el usuario",
                     error = string.Join(", ", identityUser.Errors)
                 });
-                resp.StatusCode = 400;
-                return resp;
             }
 
             user = await _userManager.FindByNameAsync(model.Username);
