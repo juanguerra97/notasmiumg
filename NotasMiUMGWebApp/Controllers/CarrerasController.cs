db2bdbe3f82503a8013ad61a5c8b957dac7f1ca8
Juan Guerra <juanguerra97@protonmail.com>
2019-12-06 18:04:08 -0600

    Se detecta duplicado capturando excepcion

--- NotasMiUMGWebApp/Controllers/CarrerasController.cs
+++ NotasMiUMGWebApp/Controllers/CarrerasController.cs
@@ -17,6 +17,7 @@ namespace NotasMiUMGWebApp.Controllers
     public class CarrerasController : Controller
     {
 
+        private readonly Regex DUP_KEY_ERROR_REGEX = new Regex(@"^Duplicate entry '.*' for key '(.*)'.*$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
         private readonly Regex FK_ERROR_REGEX = new Regex(@"^Cannot delete or update a parent row: a foreign key constraint fails.*$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
         private readonly ApplicationDbContext _context;
@@ -46,7 +47,8 @@ namespace NotasMiUMGWebApp.Controllers
         [Route("{codigo}")]
         public async Task<IActionResult> Get([FromRoute] uint codigo)
         {
-            var carrera = _context.Carreras.Select(c => new { c.CodigoCarrera, c.NombreCarrera })
+            var carrera = _context.Carreras
+                .Select(c => new { c.CodigoCarrera, c.NombreCarrera })
                 .FirstOrDefault(c => c.CodigoCarrera == codigo);
             if(carrera != null)
             {
@@ -58,13 +60,12 @@ namespace NotasMiUMGWebApp.Controllers
             }
             else
             {
-                JsonResult resp = Json(new {
+                return BadRequest(new
+                {
                     status = 400,
                     message = "No se encontró la carrera",
                     error = "La carrera no existe"
                 });
-                resp.StatusCode = 400;
-                return resp;
             }
             
         }
@@ -75,34 +76,35 @@ namespace NotasMiUMGWebApp.Controllers
         public async Task<IActionResult> Create([FromBody] Carrera model)
         {
 
-            var idUsuario = User.Claims.Where(a => a.Type == ClaimTypes.NameIdentifier).FirstOrDefault().Value;
-            bool esAdmin = await _userManager.IsInRoleAsync(await _userManager.FindByIdAsync(idUsuario), "ADMIN");
-            if (!esAdmin)
+            if (!(await esAdmin()))
             {
                 return Forbid();
             }
 
-            var carrera = await _context.Carreras.FirstOrDefaultAsync(c => c.CodigoCarrera == model.CodigoCarrera);
-            if(carrera != null)
-            {
-                JsonResult resp = Json(new { 
-                    status = 400,
-                    message = "No se guardó la carrera", 
-                    error = "Código duplicado" });
-                resp.StatusCode = 400;
-                return resp;
-            }
+            //var carrera = await _context.Carreras
+            //    .FirstOrDefaultAsync(c => c.CodigoCarrera == model.CodigoCarrera);
+            //if(carrera != null)
+            //{
+            //    return BadRequest(new
+            //    {
+            //        status = 400,
+            //        message = "No se guardó la carrera",
+            //        error = "Código duplicado"
+            //    });
+            //}
 
-            carrera = await _context.Carreras.FirstOrDefaultAsync(c => c.NombreCarrera.Equals(model.NombreCarrera, StringComparison.InvariantCultureIgnoreCase));
-            if (carrera != null)
-            {
-                JsonResult resp = Json(new { 
-                    status = 400,
-                    message = "No se guardó la carrera", 
-                    error = "Nombre duplicado" });
-                resp.StatusCode = 400;
-                return resp;
-            }
+            //carrera = await _context.Carreras
+            //    .FirstOrDefaultAsync(c => c.NombreCarrera.Equals(model.NombreCarrera, 
+            //        StringComparison.InvariantCultureIgnoreCase));
+            //if (carrera != null)
+            //{
+            //    return BadRequest(new
+            //    {
+            //        status = 400,
+            //        message = "No se guardó la carrera",
+            //        error = "Nombre duplicado"
+            //    });
+            //}
             try
             {
                 await _context.Carreras.AddAsync(model);
@@ -112,12 +114,26 @@ namespace NotasMiUMGWebApp.Controllers
                     message = "Carrera guardada" });
             } catch(DbUpdateException ex)
             {
-                JsonResult resp = Json(new { 
+                string error = ex.InnerException.Message;
+                MatchCollection groups = DUP_KEY_ERROR_REGEX.Matches(error);
+
+                if (groups.Count > 0)
+                {
+                    var match = groups[0];
+                    if (string.Compare(match.Groups[1].Value,"PRIMARY") == 0)
+                    {
+                        error = "Codigo duplicado";
+                    } else
+                    {
+                        error = "Nombre duplicado";
+                    }
+                }
+                return BadRequest(new
+                {
                     status = 400,
-                    message = "No se guardó la carrera", 
-                    error = ex.InnerException.Message });
-                resp.StatusCode = 400;
-                return resp;
+                    message = "No se guardó la carrera",
+                    error
+                });
             }
             
         }
@@ -127,10 +143,6 @@ namespace NotasMiUMGWebApp.Controllers
         [Route("{CodigoCarrera}")]
         public async Task<IActionResult> Eliminar([FromRoute] uint codigoCarrera)
         {
-            if (!(await esAdmin()))
-            {
-                return Forbid();
-            }
 
             try
             {
