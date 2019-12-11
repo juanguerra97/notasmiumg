25f57ca2e317463be7e2585861a346047ca8d07c
Juan Guerra <juanguerra97@protonmail.com>
2019-12-11 11:06:19 -0600

    Modelo para actualizar un pensum-curso

---
+++ NotasMiUMGWebApp/Models/PensumCursoModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NotasMiUMGWebApp.Models
+{
+    public class PensumCursoModel
+    {
+
+        [Required(ErrorMessage = "Falta el código de carrera")]
+        public uint CodigoCarrera { get; set; }
+
+        [Required(ErrorMessage = "Falta el año del pensum")]
+        public uint AnoPensum { get; set; }
+
+        [Required(ErrorMessage = "Falta el código del curso")]
+        public uint CodigoCurso { get; set; }
+
+        [Required(ErrorMessage = "Falta el ciclo")]
+        public byte Ciclo { get; set; }
+
+        [Required(ErrorMessage = "Faltan los créditos")]
+        public byte Creditos { get; set; }
+
+
+    }
+}
