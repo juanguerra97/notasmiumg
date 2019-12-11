06a1f0552c3c0219c5a13d0d72b14d24ab01c5df
Juan Guerra <juanguerra97@protonmail.com>
2019-12-10 18:23:01 -0600

    Modelo para un curso

---
+++ NotasMiUMGWebApp/Models/CursoModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NotasMiUMGWebApp.Models
+{
+    public class CursoModel
+    {
+
+        [Required(ErrorMessage = "Falta el código de la carrera")]
+        public uint CodigoCarrera { get; set; }
+
+        [Required(ErrorMessage = "Falta el código del curso")]
+        public uint CodigoCurso { get; set; }
+
+        [Required(ErrorMessage = "Falta el nombre del curso")]
+        public string NombreCurso { get; set; }
+
+    }
+}
