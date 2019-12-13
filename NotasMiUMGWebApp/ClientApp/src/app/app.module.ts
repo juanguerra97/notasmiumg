cf8e7a32622322fd8d7a4b69d2e163ba40dc1700
Juan Guerra <juanguerra97@protonmail.com>
2019-12-13 17:57:57 -0600

    Componente para la pantalla de login

--- NotasMiUMGWebApp/ClientApp/src/app/app.module.ts
+++ NotasMiUMGWebApp/ClientApp/src/app/app.module.ts
@@ -7,12 +7,16 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+import {NotLogedInGuard} from './not-loged-in.guard';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -20,6 +24,7 @@ import { HomeComponent } from './home/home.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'login', component: LoginComponent, canActivate: [NotLogedInGuard] }
     ])
   ],
   providers: [
