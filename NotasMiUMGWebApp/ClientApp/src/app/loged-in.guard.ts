9ea251b697479392de800ccb30cb9db1d55a8d43
Juan Guerra <juanguerra97@protonmail.com>
2019-12-13 12:31:44 -0600

    Guard para proteger componentes para los que se necesita estar autenticado

---
+++ NotasMiUMGWebApp/ClientApp/src/app/loged-in.guard.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, CanActivateChild, CanLoad, Route, Router, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './services/authorization/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LogedInGuard implements CanActivate, CanActivateChild, CanLoad {
+
+  constructor(private router: Router, private auth: AuthService) { }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.isAuthorized();
+  }
+
+  canActivateChild(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.isAuthorized();
+  }
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
+    return this.isAuthorized();
+  }
+
+  private isAuthorized(): boolean {
+    const authorized: boolean = this.auth.isAuthenticated();
+    if(!authorized) {
+      this.router.navigateByUrl('/login');
+    }
+    return authorized;
+  }
+
+}
