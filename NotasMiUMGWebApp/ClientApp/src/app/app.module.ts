114d9b800df10d6c52a2f6d980e1485ddd472e98
Juan Guerra <juanguerra97@protonmail.com>
2019-12-14 17:08:13 -0600

    Pantalla basica de registro

--- NotasMiUMGWebApp/ClientApp/src/app/app.module.ts
+++ NotasMiUMGWebApp/ClientApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
@@ -21,14 +21,15 @@ import { NotLogedInGuard } from './not-loged-in.guard';
     RegistroComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    BrowserModule.withServerTransition({appId: 'ng-cli-universal'}),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'login', component: LoginComponent, canActivate: [NotLogedInGuard] },
-      { path: 'registro', component: RegistroComponent, canActivate: [NotLogedInGuard] },
-    ])
+      {path: '', component: HomeComponent, pathMatch: 'full'},
+      {path: 'login', component: LoginComponent, canActivate: [NotLogedInGuard]},
+      {path: 'registro', component: RegistroComponent, canActivate: [NotLogedInGuard]},
+    ]),
+    ReactiveFormsModule
   ],
   providers: [
     /*{ provide: HTTP_INTERCEPTORS, useClass: AuthorizeInterceptor, multi: true }*/
--- NotasMiUMGWebApp/ClientApp/src/app/registro/registro.component.ts
+++ NotasMiUMGWebApp/ClientApp/src/app/registro/registro.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {CarreraService} from '../services/carrera.service';
+import {PensumService} from '../services/pensum.service';
+import {Carrera} from '../model/Carrera';
+import ServerResponse from '../model/ServerResponse';
+import {Router} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +15,86 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  public carreras: Carrera[] = [];
+  public pensums: number[] = [];
+
+  public formRegistro = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(14),
+      Validators.email
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6)
+    ]),
+    password_rep: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6)
+    ]),
+    nombre: new FormControl('', [
+      Validators.required
+    ]),
+    apellido: new FormControl('', [
+      Validators.required
+    ]),
+    carne: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^\\d{4}-\\d{2}-\\d{1,8}$')
+    ]),
+    codigoCarrera: new FormControl('', [
+      Validators.required,
+    ]),
+    anoPensum: new FormControl('', [
+      Validators.required
+    ]),
+    anoInicio: new FormControl('', [
+      Validators.required
+    ])
+  });
+
+  constructor(
+    private carreraService: CarreraService,
+    private pensumService: PensumService,
+    private router: Router,
+    private http: HttpClient
+  ) { }
 
   ngOnInit() {
+    this.cargarCarreras();
   }
+
+  public registrar(): void {
+    let datos = this.formRegistro.value;
+    datos.codigoCarrera = datos.codigoCarrera * 1;
+    datos.anoPensum = datos.anoPensum * 1;
+    console.log(datos);
+    if(datos.password != datos.password_rep) return;
+    this.http.post<ServerResponse>(`${environment.apiBaseUrl}/api/estudiante/signup`, datos)
+      .subscribe((res: ServerResponse) => {
+        if(res.status == 200) {
+          this.router.navigateByUrl('/login');
+        }
+      }, console.error);
+  }
+
+  public cargarCarreras(): void {
+    this.carreraService.getAll()
+      .subscribe((res: ServerResponse) => {
+        if(res.status == 200) {
+          this.carreras = res.data;
+        }
+      }, console.error);
+  }
+
+  public cargarPensums(): void {
+    this.pensumService.getAllByCarrera(this.formRegistro.value.codigoCarrera)
+      .subscribe((res: ServerResponse) => {
+        if(res.status == 200) {
+          this.pensums = res.data.pensums;
+        }
+      }, console.error);
+  }
+
 
 }
