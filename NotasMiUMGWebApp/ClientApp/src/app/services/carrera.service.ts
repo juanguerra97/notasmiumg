0ff529f4733d0c9b0fdf07b83c4b61762a8871df
Juan Guerra <juanguerra97@protonmail.com>
2019-12-14 15:10:07 -0600

    Metodo para obtener todas las carreras en el service de carreras

--- NotasMiUMGWebApp/ClientApp/src/app/services/carrera.service.ts
+++ NotasMiUMGWebApp/ClientApp/src/app/services/carrera.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import ServerResponse from '../model/ServerResponse';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarreraService {
 
-  constructor() { }
+  private static CARRERA_API_URL = `${environment.apiBaseUrl}/carreras`;
+
+  constructor(private http: HttpClient) { }
+
+  public getAll(): Observable<ServerResponse> {
+    return this.http.get<ServerResponse>(CarreraService.CARRERA_API_URL);
+  }
+
 }
