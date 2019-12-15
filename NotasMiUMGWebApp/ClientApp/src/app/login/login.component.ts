c0089b6786eea002db4d25c83c83f1cf52fec77f
Juan Guerra <juanguerra97@protonmail.com>
2019-12-14 18:31:03 -0600

    Pantalla básica de login

--- NotasMiUMGWebApp/ClientApp/src/app/login/login.component.ts
+++ NotasMiUMGWebApp/ClientApp/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {AuthService} from '../services/authorization/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public loginForm = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.email,
+      Validators.minLength(14)
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6)
+    ]),
+  });
+
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+
+  }
+
+  public iniciarSesion(): void {
+    const { username, password } = this.loginForm.value;
+    this.auth.iniciarSesion(username, password)
+      .then((res: any) => {
+        console.log(`¡Bienvenido ${res.username}!`);
+        this.router.navigateByUrl('/');
+      })
+      .catch(console.error);
   }
 
 }
