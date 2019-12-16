28aeb5fcd1e088b116080a4de13b7e39e666dad0
Juan Guerra <juanguerra97@protonmail.com>
2019-12-16 17:55:03 -0600

    Guard para proteger las rutas de pantallas del administrador

---
+++ NotasMiUMGWebApp/ClientApp/src/app/admin.guard.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  CanActivateChild,
+  CanLoad,
+  Route,
+  UrlSegment,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import {AuthService} from './services/authorization/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuard implements CanActivate, CanActivateChild, CanLoad {
+
+  constructor(private router: Router, private auth: AuthService) { }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.isAdmin();
+  }
+
+  canActivateChild(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.isAdmin();
+  }
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
+    return this.isAdmin();
+  }
+
+  private isAdmin(): boolean {
+    const admin: boolean = this.auth.isAdmin();
+    if(!admin) {
+      this.router.navigateByUrl('/');
+    }
+    return admin;
+  }
+
+}
