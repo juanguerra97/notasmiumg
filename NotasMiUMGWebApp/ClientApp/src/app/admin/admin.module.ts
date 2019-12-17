f7d4b2231503a492072ba34a284be6973052d31a
Juan Guerra <juanguerra97@protonmail.com>
2019-12-16 18:20:23 -0600

    Creacion de componente CarrerasComponent

--- NotasMiUMGWebApp/ClientApp/src/app/admin/admin.module.ts
+++ NotasMiUMGWebApp/ClientApp/src/app/admin/admin.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminRoutes } from './admin.routes';
+import { CarrerasComponent } from './carreras/carreras.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [CarrerasComponent],
   imports: [
     CommonModule,
     AdminRoutes
--- NotasMiUMGWebApp/ClientApp/src/app/admin/admin.routes.ts
+++ NotasMiUMGWebApp/ClientApp/src/app/admin/admin.routes.ts
@@ -1,8 +1,17 @@
 import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
+import { CarrerasComponent } from './carreras/carreras.component';
 
 export const ADMIN_ROUTES: Routes = [
-
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'carreras'
+  },
+  {
+    path: 'carreras',
+    component: CarrerasComponent
+  }
 ];
 
 @NgModule({
