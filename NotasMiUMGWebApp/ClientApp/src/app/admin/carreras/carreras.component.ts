603805f4e9c1ce53e3ef27dabe2350014774c94c
Juan Guerra <juanguerra97@protonmail.com>
2019-12-16 18:42:05 -0600

    Se muestra tabla con todas las carreras

--- NotasMiUMGWebApp/ClientApp/src/app/admin/carreras/carreras.component.ts
+++ NotasMiUMGWebApp/ClientApp/src/app/admin/carreras/carreras.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CarreraService } from '../../services/carrera.service';
+import { Carrera } from '../../model/Carrera';
+import ServerResponse from '../../model/ServerResponse';
 
 @Component({
   selector: 'app-carreras',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarrerasComponent implements OnInit {
 
-  constructor() { }
+  public carreras: Carrera[] = [];
+
+  constructor(private carreraService: CarreraService) { }
 
   ngOnInit() {
+    this.carreraService.getAll()
+      .subscribe((res: ServerResponse) => {
+        if(res.status == 200) {
+          this.carreras = res.data;
+        }
+      }, console.error);
   }
 
 }
