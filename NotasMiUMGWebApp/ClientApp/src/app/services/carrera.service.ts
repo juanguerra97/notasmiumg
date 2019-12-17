ba6b0b0b441d6fc56ae59423e598bf4410bb2a63
Juan Guerra <juanguerra97@protonmail.com>
2019-12-16 18:38:54 -0600

    Se implementaron los metodos para crear y eliminar carreras

--- NotasMiUMGWebApp/ClientApp/src/app/services/carrera.service.ts
+++ NotasMiUMGWebApp/ClientApp/src/app/services/carrera.service.ts
@@ -3,18 +3,27 @@ import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import ServerResponse from '../model/ServerResponse';
+import {Carrera} from '../model/Carrera';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarreraService {
 
-  private static CARRERA_API_URL = `${environment.apiBaseUrl}/api/carreras`;
+  private static readonly CARRERA_API_URL = `${environment.apiBaseUrl}/api/carreras`;
 
   constructor(private http: HttpClient) { }
 
   public getAll(): Observable<ServerResponse> {
     return this.http.get<ServerResponse>(CarreraService.CARRERA_API_URL);
+  }
+
+  public create(carrera: Carrera): Observable<ServerResponse> {
+    return this.http.post<ServerResponse>(CarreraService.CARRERA_API_URL, carrera);
+  }
+
+  public delete(codigoCarrera: number): Observable<ServerResponse> {
+    return this.http.delete<ServerResponse>(`${CarreraService.CARRERA_API_URL}/${codigoCarrera}`);
   }
 
 }
