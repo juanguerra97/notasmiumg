458b0d088c7ab398a1095df277b15b70100c271e
Juan Guerra <juanguerra97@protonmail.com>
2019-12-16 18:56:07 -0600

    Importacion del modulo FontAwesome

--- NotasMiUMGWebApp/ClientApp/src/app/admin/admin.module.ts
+++ NotasMiUMGWebApp/ClientApp/src/app/admin/admin.module.ts
@@ -4,12 +4,14 @@ import { AdminRoutes } from './admin.routes';
 import { CarrerasComponent } from './carreras/carreras.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { PensumsComponent } from './pensums/pensums.component';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [CarrerasComponent, CursosComponent, PensumsComponent],
   imports: [
     CommonModule,
-    AdminRoutes
+    AdminRoutes,
+    FontAwesomeModule
   ]
 })
 export class AdminModule { }
