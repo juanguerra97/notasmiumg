72cb0f3c649e52827df0b745ebc0dda514aca91e
Juan Guerra <juanguerra97@protonmail.com>
2019-12-17 17:31:52 -0600

    Importacion de FormsModule y ReactiveFormsModule

--- NotasMiUMGWebApp/ClientApp/src/app/admin/admin.module.ts
+++ NotasMiUMGWebApp/ClientApp/src/app/admin/admin.module.ts
@@ -5,13 +5,16 @@ import { CarrerasComponent } from './carreras/carreras.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { PensumsComponent } from './pensums/pensums.component';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [CarrerasComponent, CursosComponent, PensumsComponent],
   imports: [
     CommonModule,
     AdminRoutes,
-    FontAwesomeModule
+    FontAwesomeModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class AdminModule { }
