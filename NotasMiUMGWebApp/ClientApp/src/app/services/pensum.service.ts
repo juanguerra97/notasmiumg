59b7c34a1fed0e0db66541529900d50aeb601022
Juan Guerra <juanguerra97@protonmail.com>
2019-12-17 18:48:05 -0600

    Metodos para crear y eliminar pensums en el service para pensums

--- NotasMiUMGWebApp/ClientApp/src/app/services/pensum.service.ts
+++ NotasMiUMGWebApp/ClientApp/src/app/services/pensum.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import ServerResponse from '../model/ServerResponse';
+import Pensum from '../model/Pensum';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +16,14 @@ export class PensumService {
 
   public getAllByCarrera(codigoCarrera: number): Observable<ServerResponse> {
     return this.http.get<ServerResponse>(`${PensumService.PENSUM_API_URL}/${codigoCarrera}`);
+  }
+
+  public create(pensum: Pensum): Observable<ServerResponse> {
+    return this.http.post<ServerResponse>(PensumService.PENSUM_API_URL, pensum);
+  }
+
+  public delete(codigoCarrera: number, anoPensum: number): Observable<ServerResponse> {
+    return this.http.delete<ServerResponse>(`${PensumService.PENSUM_API_URL}/${codigoCarrera}/${anoPensum}`);
   }
 
 }
