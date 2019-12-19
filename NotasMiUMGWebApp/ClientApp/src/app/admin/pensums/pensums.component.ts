5b5a70868780094930987da7b3776454a3c4c4cf
Juan Guerra <juanguerra97@protonmail.com>
2019-12-19 11:21:59 -0600

    Se pueden agregar cursos a los pensum

--- NotasMiUMGWebApp/ClientApp/src/app/admin/pensums/pensums.component.ts
+++ NotasMiUMGWebApp/ClientApp/src/app/admin/pensums/pensums.component.ts
@@ -3,10 +3,12 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { faTrash, faSave } from '@fortawesome/free-solid-svg-icons';
 import { CarreraService } from '../../services/carrera.service';
 import { CursoService } from '../../services/curso.service';
+import { PensumCursoService } from '../../services/pensum-curso.service';
 import { PensumService } from '../../services/pensum.service';
 import ServerResponse from '../../model/ServerResponse';
 import { Carrera } from '../../model/Carrera';
-
+import { Curso } from '../../model/Curso';
+import { PensumCurso } from '../../model/PensumCurso';
 
 @Component({
   selector: 'app-pensums',
@@ -18,11 +20,16 @@ export class PensumsComponent implements OnInit {
   faTrash = faTrash;
   faSave = faSave;
 
-  private carreras: Carrera[] = [];
-  private codigoCarreraSel = 0;
+  public carreras: Carrera[] = [];
+  public codigoCarreraSel = 0;
 
-  private pensums: number[] = [];
-  private pensumSel: number = null;
+  public pensums: number[] = [];
+  public pensumSel: number = null;
+
+  public cursos: Curso[] = [];
+
+  public pensumCursos: PensumCurso[] = [];
+  public pensumCursoSel: PensumCurso = null;
 
   public formNewPensum = new FormGroup({
     anoPensum: new FormControl('', [
@@ -31,9 +38,31 @@ export class PensumsComponent implements OnInit {
     ])
   });
 
+  public formNewPensumCurso = new FormGroup({
+    codigoCurso: new FormControl('',[
+      Validators.required,
+      Validators.pattern("^\\d+$")
+    ]),
+    ciclo: new FormControl('', [
+      Validators.required,
+      Validators.min(1),
+      Validators.max(100),
+      Validators.pattern("^\\d+$")
+    ]),
+    creditos: new FormControl('', [
+      Validators.required,
+      Validators.min(0),
+      Validators.max(100),
+      Validators.pattern("^\\d+$")
+    ])
+  });
+
+  public verCursosChecked = false;
+
   constructor(
     private carreraService: CarreraService,
     private cursoService: CursoService,
+    private pensumCursoService: PensumCursoService,
     private pensumService: PensumService
   ) { }
 
@@ -70,6 +99,49 @@ export class PensumsComponent implements OnInit {
       }, console.error);
   }
 
+  public crearPensumCurso(): void {
+
+    if(this.codigoCarreraSel == 0
+      || this.pensumSel == null) return;
+
+    const newPensumCurso = this.formNewPensumCurso.value;
+    newPensumCurso.codigoCarrera = this.codigoCarreraSel * 1;
+    newPensumCurso.anoPensum = this.pensumSel;
+    newPensumCurso.codigoCurso *= 1;
+    newPensumCurso.ciclo *= 1;
+    newPensumCurso.creditos *= 1;
+
+    this.pensumCursoService.create(newPensumCurso)
+      .subscribe((res: ServerResponse) => {
+        if(res.status == 200) {
+          this.cargarPensumCursosDePensum();
+          this.formNewPensumCurso.reset();
+        }
+      }, console.error);
+  }
+
+  public eliminarPensumCurso(): void {
+    if(this.codigoCarreraSel == 0
+      || this.pensumSel == null
+      || this.pensumCursoSel == null) return;
+    this.pensumCursoService.delete(this.codigoCarreraSel, this.pensumSel, this.pensumCursoSel.codigoCurso)
+      .subscribe((res: ServerResponse) => {
+        if(res.status == 200) {
+          this.pensumCursos.splice(this.pensumCursos.findIndex(c => c.codigoCurso == this.pensumCursoSel.codigoCurso), 1);
+          this.pensumCursoSel = null;
+        }
+      }, console.error);
+  }
+
+  public changeCarrera(): void {
+    this.cargarPensums();
+    if(this.verCursosChecked) {
+      this.cargarCursos();
+    } else {
+      this.cursos = [];
+    }
+  }
+
   public cargarPensums(): void {
     this.pensumSel = null;
     this.pensumService.getAllByCarrera(this.codigoCarreraSel)
@@ -92,9 +164,49 @@ export class PensumsComponent implements OnInit {
   public clickPensum(pensum: number): void {
     if(this.pensumSel == pensum) {
       this.pensumSel = null;
+      this.pensumCursos = [];
+      this.pensumCursoSel = null;
     } else {
       this.pensumSel = pensum;
+      this.cargarPensumCursosDePensum();
     }
+  }
+
+  public changeVerPensumCursos(): void {
+    this.pensumCursoSel = null;
+    this.pensumCursos = [];
+    if(this.verCursosChecked) {
+      this.cargarCursos();
+      if(this.pensumSel != null) {
+        this.cargarPensumCursosDePensum();
+      }
+    }
+  }
+
+  public cargarPensumCursosDePensum(): void {
+    this.pensumCursoService.getAllByPensum(this.codigoCarreraSel, this.pensumSel)
+      .subscribe((res: ServerResponse) => {
+        if(res.status == 200) {
+          this.pensumCursos = res.data;
+        }
+      }, console.error);
+  }
+
+  public clickPensumCurso(pensumCurso: PensumCurso): void {
+    if(this.pensumCursoSel == pensumCurso) {
+      this.pensumCursoSel = null;
+    } else {
+      this.pensumCursoSel = pensumCurso;
+    }
+  }
+
+  public cargarCursos(): void {
+    this.cursoService.getAll(this.codigoCarreraSel)
+      .subscribe((res: ServerResponse) => {
+        if(res.status == 200) {
+          this.cursos = res.data;
+        }
+      }, console.error);
   }
 
 }
