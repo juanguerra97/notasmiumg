5186612fe173df9d7ef83584721ea4c0a9e0079a
Juan Guerra <juanguerra97@protonmail.com>
2019-12-20 10:34:45 -0600

    Creacion modlo Estudiante

---
+++ NotasMiUMGWebApp/ClientApp/src/app/estudiante/estudiante.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FontAwesomeModule
+  ]
+})
+export class EstudianteModule { }
