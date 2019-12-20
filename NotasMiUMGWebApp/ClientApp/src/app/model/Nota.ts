bbdcb33ed7c621ffece479caa6d835f97eeec28b
Juan Guerra <juanguerra97@protonmail.com>
2019-12-20 11:30:42 -0600

    Interfaz para representar una nota

---
+++ NotasMiUMGWebApp/ClientApp/src/app/model/Nota.ts
@@ -0,0 +1,17 @@
+
+export interface Nota {
+  estudianteId?: number;
+  codigoCarrera?: number;
+  anoPensum?: number;
+  codigoCurso?: number;
+  nombreCurso?: number;
+  ano: number;
+  ciclo?: number;
+  primerParcial: number;
+  segundoParcial: number;
+  actividades: number;
+  zona?: number;
+  examenFinal: number;
+  notaFinal?: number;
+  aprobado?: boolean;
+}
