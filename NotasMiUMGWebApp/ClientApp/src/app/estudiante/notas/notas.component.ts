92216117a9761e1122a18206fdfacc1fa27f099b
Juan Guerra <juanguerra97@protonmail.com>
2019-12-26 12:40:50 -0600

    Se muestra las notas filtradas por ano y ciclo

--- NotasMiUMGWebApp/ClientApp/src/app/estudiante/notas/notas.component.ts
+++ NotasMiUMGWebApp/ClientApp/src/app/estudiante/notas/notas.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { faPlus, faTrashAlt, faFilter } from '@fortawesome/free-solid-svg-icons';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Nota } from '../../model/Nota';
+import { NotaService } from '../../services/nota.service';
+import ServerResponse from '../../model/ServerResponse';
 
 @Component({
   selector: 'app-notas',
@@ -7,9 +12,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotasComponent implements OnInit {
 
-  constructor() { }
+  faPlus = faPlus;
+  faTrashAlt = faTrashAlt;
+  faFilter = faFilter;
+
+  public notas: Nota[] = [];
+  public notaSel: Nota = null;
+
+  public filtroModificado = false;
+
+  public formVerNotas = new FormGroup({
+    ano: new FormControl('',[
+      Validators.required,
+      Validators.pattern('^\\d+$')
+    ]),
+    ciclo: new FormControl('',[
+      Validators.required,
+      Validators.pattern('^\\d+$')
+    ])
+  });
+
+  constructor(
+    private notaService: NotaService
+  ) { }
 
   ngOnInit() {
+  }
+
+  public imp(val: any): void {
+    console.log(val);
+  }
+
+  public verNotas(): void {
+    this.cargarNotas();
+    this.filtroModificado = false;
+  }
+
+  public cargarNotas(): void {
+    if(!this.formVerNotas.valid) return;
+    const {ano: ano, ciclo: ciclo} = this.formVerNotas.value;
+    this.notaService.getAllByCiclo(ano, ciclo)
+      .subscribe((res: ServerResponse) => {
+        if(res.status == 200) {
+          this.notas = res.data;
+        }
+      }, console.error);
+  }
+
+  public cambioFiltro(): void {
+    this.filtroModificado = true;
   }
 
 }
