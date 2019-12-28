dd548544f9ea190ca5103a8378f0e43ba219320f
Juan Guerra <juanguerra97@protonmail.com>
2019-12-28 12:15:27 -0600

    Interfaz para representar los datos de un estudiante

---
+++ NotasMiUMGWebApp/ClientApp/src/app/model/Estudiante.ts
@@ -0,0 +1,11 @@
+
+export interface Estudiante {
+  estudianteId?: number;
+  carne: string;
+  nombre: string;
+  apellido: string;
+  correo: string;
+  codigoCarrera: number;
+  anoPensum: number;
+  anoInicio: number;
+}
