07c0d99e64101cc68887344d1f652e59e0a8ac7a
Juan Guerra <juanguerra97@protonmail.com>
2020-01-03 11:54:05 -0600

    Se muestra promedio general y graficos de promedios anual y semestral

--- NotasMiUMGWebApp/ClientApp/src/app/estudiante/estadisticas/estadisticas.component.ts
+++ NotasMiUMGWebApp/ClientApp/src/app/estudiante/estadisticas/estadisticas.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { EstadisticaService } from '../../services/estadistica.service';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Label } from 'ng2-charts';
+import ServerResponse from '../../model/ServerResponse';
 
 @Component({
   selector: 'app-estadisticas',
@@ -7,9 +11,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EstadisticasComponent implements OnInit {
 
-  constructor() { }
+  public promedioGeneral = 0;
+
+  promedioAnualChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  promedioAnualChartLabels: Label[] = [];
+  promedioAnualChartType: ChartType = 'bar';
+  promedioAnualChartLegend = true;
+  promedioAnualChartPlugins = [];
+  promedioAnualChartData: ChartDataSets[] = [
+  ];
+  public mostrarChartPromedioAnual = false;
+
+  promedioSemestralChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  promedioSemestralChartLabels: Label[] = [];
+  promedioSemestralChartType: ChartType = 'line';
+  promedioSemestralChartLegend = true;
+  promedioSemestralChartPlugins = [];
+  promedioSemestralChartData: ChartDataSets[] = [];
+  public mostrarChartPromedioSemestral = false;
+
+  constructor(private estadisticaService: EstadisticaService) { }
 
   ngOnInit() {
+    this.estadisticaService.getPromedios()
+      .subscribe((res: ServerResponse) => {
+        if(res.status == 200) {
+          this.promedioGeneral = res.data.general;
+          this.crearChartPromedioAnual(res.data.anual);
+          this.crearChartPromedioSemestral(res.data.semestral);
+        }
+      }, console.error);
   }
+
+  private crearChartPromedioAnual(promediosAnual: any[]): void {
+    this.promedioAnualChartLabels = promediosAnual.map(p => p.ano);
+    this.promedioAnualChartData = [{data: promediosAnual.map(p => p.promedio), label: 'Promedio anual'}];
+    this.mostrarChartPromedioAnual = true;
+  }
+
+  private crearChartPromedioSemestral(promediosSemestral: any[]): void {
+    promediosSemestral = promediosSemestral.map(r => r.semestres.map(p =>{ return { ano: r.ano, semestre: p.semestre, promedio: p.promedio }})).reduce((acc, curr) => { acc.push(...curr); return acc;}, []);
+    this.promedioSemestralChartLabels = promediosSemestral.map(p => `${p.semestre}/${p.ano}`);
+    this.promedioSemestralChartData = [ {data: promediosSemestral.map(p => p.promedio), label: 'Promedio semestral'}];
+    this.mostrarChartPromedioSemestral = true;
+  }
+
 
 }
