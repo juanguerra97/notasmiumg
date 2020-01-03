cf4853f70aad519d86699c5a8a79f639ed2bfee0
Juan Guerra <juanguerra97@protonmail.com>
2020-01-03 10:23:39 -0600

    Creacion del servicio EstadisticaService con metodo para obtener los promedios

---
+++ NotasMiUMGWebApp/ClientApp/src/app/services/estadistica.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import ServerResponse from '../model/ServerResponse';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EstadisticaService {
+
+  private static readonly ESTADISTICA_API_URL = `${environment.apiBaseUrl}/api/estadisticas`;
+
+  constructor(private http: HttpClient) { }
+
+  public getPromedios(codigoCarrera: number): Observable<ServerResponse> {
+    return this.http.get<ServerResponse>(`${EstadisticaService.ESTADISTICA_API_URL}/promedios`);
+  }
+
+}
