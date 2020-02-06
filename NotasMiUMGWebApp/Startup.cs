817c0ef11f8c64dd26f1b4835c0b48d1e69e200d
Juan Guerra <juanguerra97@protonmail.com>
2020-02-06 16:17:45 -0600

    Ingreso de datos iniciales

--- NotasMiUMGWebApp/Startup.cs
+++ NotasMiUMGWebApp/Startup.cs
@@ -14,6 +14,8 @@ using System;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using System.Threading.Tasks;
+using jguerra.notasmiumg;
 
 namespace NotasMiUMGWebApp
 {
@@ -39,7 +41,7 @@ namespace NotasMiUMGWebApp
 
             services.AddIdentityServer()
                 .AddApiAuthorization<IdentityUser, ApplicationDbContext>();
-
+            
             services.AddAuthentication(options =>
             {
                 options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -97,15 +99,15 @@ namespace NotasMiUMGWebApp
                 "abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ0123456789-._@+";
                 options.User.RequireUniqueEmail = true;
             });
-
+            
             services.AddScoped<IAuthorizationHandler, AdminAuthorizationHandler>();
             services.AddScoped<IAuthorizationHandler, EstudianteAuthorizationHandler>();
             services.AddScoped<IAuthorizationHandler, NotaAuthorizationHandler>();
-
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -118,7 +120,7 @@ namespace NotasMiUMGWebApp
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             if (!env.IsDevelopment())
@@ -151,6 +153,142 @@ namespace NotasMiUMGWebApp
                     spa.UseAngularCliServer(npmScript: "start");
                 }
             });
+
+            var scope = app.ApplicationServices.CreateScope();
+            await InitRoles(scope.ServiceProvider.GetService<RoleManager<IdentityRole>>());
+            await InitAdmin(scope.ServiceProvider.GetService<UserManager<IdentityUser>>());
+            await InitData(scope.ServiceProvider.GetService<ApplicationDbContext>());
         }
+
+        // metodo para crear los roles de la aplicacion si no existen
+        private static async Task InitRoles(RoleManager<IdentityRole> rm)
+        {
+            var admin = await rm.FindByNameAsync("ADMIN");
+            if(admin == null)
+            {
+                admin = new IdentityRole("ADMIN");
+                await rm.CreateAsync(admin);
+            }
+
+            var estudiante = await rm.FindByNameAsync("ESTUDIANTE");
+            if(estudiante == null)
+            {
+                estudiante = new IdentityRole("ESTUDIANTE");
+                await rm.CreateAsync(estudiante);
+            }
+
+        }
+
+        // metodo para crear al usuario administrador
+        private static async Task InitAdmin(UserManager<IdentityUser> um)
+        {
+            var admin = await um.FindByNameAsync("notasmiumg");
+
+            if(admin == null)
+            {
+                admin = new IdentityUser("notasmiumg");
+                var res = await um.CreateAsync(admin, "notasmiumg");
+                if(res.Succeeded)
+                {
+                    await um.AddToRoleAsync(admin, "ADMIN");
+                }
+            } else
+            {
+                if (!(await um.IsInRoleAsync(admin, "ADMIN")))
+                {
+                    await um.AddToRoleAsync(admin, "ADMIN");
+                }
+            }
+            
+
+        }
+
+        // metodo para guardar algunos datos
+        private static async Task InitData(ApplicationDbContext db)
+        {
+            if(0 < await db.Carreras.CountAsync()) return;
+
+            var sistemas = new Carrera(1490, "INGENIERIA EN SISTEMAS");
+            await db.Carreras.AddAsync(sistemas);
+
+            var pensum2014 = new Pensum(2014, sistemas);
+            await db.Pensums.AddAsync(pensum2014);
+
+            await db.SaveChangesAsync();
+
+            var pensumCursos = new PensumCurso[]
+            {
+                new PensumCurso(1, 4, pensum2014, new Curso(1, "DESARROLLO HUMANO Y PROFESIONAL", sistemas)),
+                new PensumCurso(1, 5, pensum2014, new Curso(2, "METODOLOGIA DE LA INVESTIGACION", sistemas)),
+                new PensumCurso(1, 5, pensum2014, new Curso(3, "CONTABILIDAD I", sistemas)),
+                new PensumCurso(1, 5, pensum2014, new Curso(4, "INTRODUCCION A LOS SISTEMAS DE COMPUTO", sistemas)),
+                new PensumCurso(1, 5, pensum2014, new Curso(5, "LOGICA DE SISTEMAS", sistemas)),
+
+                new PensumCurso(2, 5, pensum2014, new Curso(6, "PRECALCULO", sistemas)),
+                new PensumCurso(2, 5, pensum2014, new Curso(7, "ALGEBRA LINEAL", sistemas)),
+                new PensumCurso(2, 5, pensum2014, new Curso(8, "ALGORITMOS", sistemas)),
+                new PensumCurso(2, 5, pensum2014, new Curso(9, "CONTABILIDAD II", sistemas)),
+                new PensumCurso(2, 5, pensum2014, new Curso(10, "MATEMATICA DISCRETA", sistemas)),
+
+                new PensumCurso(3, 5, pensum2014, new Curso(11, "FISICA I", sistemas)),
+                new PensumCurso(3, 5, pensum2014, new Curso(12, "PROGRAMACION I", sistemas)),
+                new PensumCurso(3, 5, pensum2014, new Curso(13, "CALCULO I", sistemas)),
+                new PensumCurso(3, 4, pensum2014, new Curso(14, "PROCESO ADMINISTRATIVO", sistemas)),
+                new PensumCurso(3, 5, pensum2014, new Curso(15, "DERECHO INFORMATICO", sistemas)),
+
+                new PensumCurso(4, 5, pensum2014, new Curso(16, "MICROECONOMIA", sistemas)),
+                new PensumCurso(4, 5, pensum2014, new Curso(17, "PROGRAMACION II", sistemas)),
+                new PensumCurso(4, 5, pensum2014, new Curso(18, "CALCULO II", sistemas)),
+                new PensumCurso(4, 5, pensum2014, new Curso(19, "ESTADISTICA I", sistemas)),
+                new PensumCurso(4, 5, pensum2014, new Curso(20, "FISICA II", sistemas)),
+
+                new PensumCurso(5, 5, pensum2014, new Curso(21, "METODOS NUMERICOS", sistemas)),
+                new PensumCurso(5, 5, pensum2014, new Curso(22, "PROGRAMACION III", sistemas)),
+                new PensumCurso(5, 5, pensum2014, new Curso(23, "EMPRENDEDORES DE NEGOCIOS", sistemas)),
+                new PensumCurso(5, 5, pensum2014, new Curso(24, "ELECTRONICA ANALOGICA", sistemas)),
+                new PensumCurso(5, 5, pensum2014, new Curso(25, "ESTADISTICA II", sistemas)),
+
+                new PensumCurso(6, 5, pensum2014, new Curso(26, "INVESTIGACION DE OPERACIONES", sistemas)),
+                new PensumCurso(6, 5, pensum2014, new Curso(27, "BASES DE DATOS I", sistemas)),
+                new PensumCurso(6, 5, pensum2014, new Curso(28, "AUTOMATAS Y LENGUAJES FORMALES", sistemas)),
+                new PensumCurso(6, 5, pensum2014, new Curso(29, "SISTEMAS OPERATIVOS I", sistemas)),
+                new PensumCurso(6, 5, pensum2014, new Curso(30, "ELECTRONICA DIGITAL", sistemas)),
+
+                new PensumCurso(7, 5, pensum2014, new Curso(31, "BASES DE DATOS II", sistemas)),
+                new PensumCurso(7, 5, pensum2014, new Curso(32, "ANALISIS DE SISTEMAS I", sistemas)),
+                new PensumCurso(7, 5, pensum2014, new Curso(33, "SISTEMAS OPERATIVOS II", sistemas)),
+                new PensumCurso(7, 5, pensum2014, new Curso(34, "ARQUITECTURA DE COMPUTADORAS I", sistemas)),
+                new PensumCurso(7, 5, pensum2014, new Curso(35, "COMPILADORES", sistemas)),
+
+                new PensumCurso(8, 5, pensum2014, new Curso(36, "DESARROLLO WEB", sistemas)),
+                new PensumCurso(8, 5, pensum2014, new Curso(37, "ANALISIS DE SISTEMAS II", sistemas)),
+                new PensumCurso(8, 5, pensum2014, new Curso(38, "REDES DE COMPUTADORAS I", sistemas)),
+                new PensumCurso(8, 4, pensum2014, new Curso(39, "ETICA PROFESIONAL", sistemas)),
+                new PensumCurso(8, 5, pensum2014, new Curso(40, "ARQUITECTURA DE COMPUTADORAS II", sistemas)),
+
+                new PensumCurso(9, 5, pensum2014, new Curso(41, "ADMINISTRACION DE TECNOLOGIAS DE INFORMACION", sistemas)),
+                new PensumCurso(9, 5, pensum2014, new Curso(42, "INGENIERIA DE SOFTWARE", sistemas)),
+                new PensumCurso(9, 6, pensum2014, new Curso(43, "PROYECTO DE GRADUACION I", sistemas)),
+                new PensumCurso(9, 5, pensum2014, new Curso(44, "REDES DE COMPUTADORAS II", sistemas)),
+                new PensumCurso(9, 5, pensum2014, new Curso(45, "INTELIGENCIA ARTIFICIAL", sistemas)),
+
+                new PensumCurso(10, 5, pensum2014, new Curso(46, "TELECOMUNICACIONES", sistemas)),
+                new PensumCurso(10, 6, pensum2014, new Curso(47, "SEMINARIOS DE TECNOLOGIAS DE INFORMACION", sistemas)),
+                new PensumCurso(10, 5, pensum2014, new Curso(48, "ASEGURAMIENTO DE LA CALIDAD DEL SOFTWARE", sistemas)),
+                new PensumCurso(10, 6, pensum2014, new Curso(49, "PROYECTO DE GRADUACION II", sistemas)),
+                new PensumCurso(10, 5, pensum2014, new Curso(50, "SEGURIDAD Y AUDITORIA DE SISTEMAS", sistemas)),
+
+            };
+
+            foreach(var pc in pensumCursos)
+            {
+                db.Cursos.Add(pc.Curso);
+                db.PensumCursos.Add(pc);
+            }
+
+            await db.SaveChangesAsync();
+
+        }
+
     }
 }
